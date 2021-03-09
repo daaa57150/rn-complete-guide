a4dd7a69cc17f4a57f21bd81977059506b7bbe36
David Kern <dkern@agilepartner.net>
2021-03-09 11:38:04 +0100

    core gameplay ok

--- src/components/pages/game-page.component.tsx
+++ src/components/pages/game-page.component.tsx
@@ -2,14 +2,20 @@ import Card from '@components/common/card.component';
 import NumberContainer from '@components/common/number-container.component';
 import { buttonStyle, pageStyle, Spaces } from '@constants/styles.const';
 import * as u from '@helpers/utils';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import Button from 'react-native-button';
 
 export interface GamePageProps  {
   userChoice: number
 }
 
+export interface Bounds {
+  lower: number,
+  upper: number
+}
+
+/** Bounds included, if `answer` is provided, avoids that value */
 function guess(min: number, max: number, answer?: number): number {
   if(min === max) return min;
 
@@ -18,14 +24,47 @@ function guess(min: number, max: number, answer?: number): number {
 }
 
 export default function GamePage(props: GamePageProps) {
+  const [currentBounds, setCurrentBounds] = useState<Bounds>({ lower: 1, upper: 99 });
   const [currentGuess, setCurrentGuess] = useState(guess(1, 99, props.userChoice));
+  const [guessCount, setGuessCount] = useState(0);
+
+  // what a joke
+  useEffect(() => {
+    console.log('// effect //')
+    if(currentGuess === props.userChoice) {
+      const message = `Guessed: ${currentGuess} in ${guessCount} rounds`;
+      console.log(message);
+      // TODO: change to game over screen (hopefully there is a graceful way of doing that...)
+      Alert.alert('Game Over', `${message}\nTODO: display another screen.`);
+    }
+  });
+
+  const guessAgain = (min: number, max: number) => {
+    setCurrentBounds({ lower: min, upper: max });
+    setCurrentGuess(guess(min, max));
+    setGuessCount(guessCount + 1);
+  };
+
+  const alertNoJoke = () => {
+    Alert.alert('What?', 'Stop joking', [{ text: 'Ok', style: 'default' }])
+  };
 
   const lower = () => {
     console.log('lower!!');
+    if(props.userChoice > currentGuess) {
+      alertNoJoke();
+    } else {
+      guessAgain(currentBounds.lower, currentGuess - 1);
+    }
   };
 
   const higher = () => {
     console.log('higher!!');
+    if(props.userChoice < currentGuess) {
+      alertNoJoke();
+    } else {
+      guessAgain(currentGuess + 1, currentBounds.upper);
+    }
   };
 
   return (
